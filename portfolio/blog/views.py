061b1375f59d633050ec656eec6acf33370cd57f
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-04-08 12:16:14 +0100

    Create, update and delete posts

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import( 
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView,
+    DeleteView
+)
 from .models import post
 
 def home(request):
@@ -18,6 +25,38 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = post
 
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
